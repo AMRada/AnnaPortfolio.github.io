04e73c91d390c043ea04aa07bffc71c9720110e6
AMRada <149769881+AMRada@users.noreply.github.com>
2023-11-03 10:16:15 +0100

    juiste

---
+++ game opdrachten H2/H2005.js
@@ -0,0 +1,45 @@
+var aantalRijenRaster = 6;
+var aantalKolommenRaster = 9;
+var celGrootte;
+
+var spriteJos;
+var xJos = 400;
+var yJos = 300;
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  spriteJos = loadImage("images/sprites/Jos100px/Jos_0.png");
+}
+
+function setup() {
+  canvas = createCanvas(901,601);
+//  canvas.parent('processing');
+  celGrootte = width / aantalKolommenRaster;
+}
+
+function draw() {
+  background(brug);
+  tekenRaster();
+  image(spriteJos, xJos, yJos)
+}
+
+function tekenRaster() {
+  push();
+  noFill();
+  stroke('grey');
+  /*
+  Maak hieronder een dubbele herhaling om een raster te maken.
+  HINT: je kunt terugkijken naar het raster dat je in H1 hebt gemaakt.
+  Maak gebruik van de variabelen die bovenaan zijn gedeclareerd.
+  */
+  for (var rij = 0;rij < aantalRijenRaster;rij++) {
+    for (var kolom = 0;kolom < aantalKolommenRaster;kolom++) {
+      rect(kolom*celGrootte,rij*celGrootte,celGrootte,celGrootte);
+    }
+  }
+
+  
+      rect(4*celGrootte,2*celGrootte,celGrootte,celGrootte);
+
+  pop();
+}
